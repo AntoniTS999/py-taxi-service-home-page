10ff3b60be3d8edb4d3063c276994d566e119fc0
Anton Tsikhanovich <alluspeh@gmail.com>
2026-03-28 16:53:39 +0100

    Home page created

--- taxi/urls.py
+++ taxi/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from tkinter.font import names
 
 from django.contrib import admin
@@ -21,7 +22,6 @@ from taxi.views import index
 
 urlpatterns = [
     path("", index, name="index"),
-
 ]
 
 app_name = "taxi"
